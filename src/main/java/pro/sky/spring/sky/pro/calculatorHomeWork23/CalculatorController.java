f5d58763bae85a5920ad98b29a72121f051a86a4
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-02-26 18:01:42 +0300

    Calculator

--- src/main/java/pro/sky/spring/sky/pro/calculatorHomeWork23/CalculatorController.java
+++ src/main/java/pro/sky/spring/sky/pro/calculatorHomeWork23/CalculatorController.java
@@ -1,4 +1,34 @@
 package pro.sky.spring.sky.pro.calculatorHomeWork23;
 
-public class CalculatorController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController extends CalculatorService {
+    CalculatorService calculatorService;
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+    @GetMapping()
+    public String hello() {
+        return calculatorService.hello();
+    }
+    @GetMapping(path = "/plus")
+    public String answerSummaNumber(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return calculatorService.answerSummaNumber(num1, num2);
+    }
+    @GetMapping(path = "/minus")
+    public String answerMinusNumber(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return calculatorService.answerMinusNumber(num1, num2);
+    }
+    @GetMapping(path = "/multiply")
+    public String answerMultiplyNumber(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return calculatorService.answerMultiplyNumber(num1, num2);
+    }
+    @GetMapping(path = "/divide")
+    public String answerDivideNumber(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return calculatorService.answerDivideNumber(num1, num2);
+    }
 }
--- src/main/java/pro/sky/spring/sky/pro/calculatorHomeWork23/CalculatorService.java
+++ src/main/java/pro/sky/spring/sky/pro/calculatorHomeWork23/CalculatorService.java
@@ -1,4 +1,41 @@
 package pro.sky.spring.sky.pro.calculatorHomeWork23;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+
+    String hello() {
+        return "Добро пожаловать в калькулятор!";
+    }
+
+    public String answerSummaNumber(int num1, int num2) {
+        int res = num1 + num2;
+        return num1 + "+" + num2 + "=" + res;
+
+    }
+
+    public String answerNum2(String num2) {
+        return num2;
+    }
+
+    public String answerMinusNumber(int num1, int num2) {
+        int res = num1 - num2;
+        return num1 + "-" + num2 + "=" + res;
+    }
+
+    public String answerMultiplyNumber(int num1, int num2) {
+        int res = num1 * num2;
+        return num1 + "*" + num2 + "=" + res;
+    }
+
+    public String answerDivideNumber(int num1, int num2) {
+        int res;
+        if (num2 == 0) {
+            return "Error: на ноль делить нельзя";
+        } else {
+            res = num1 / num2;
+            return num1 + "/" + num2 + "=" + res;
+        }
+    }
 }
