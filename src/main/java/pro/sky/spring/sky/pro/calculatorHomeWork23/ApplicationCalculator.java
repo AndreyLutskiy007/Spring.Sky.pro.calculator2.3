9ab71dab6b5531f90a0050afc1a89968ff7527c7
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-02-26 17:38:38 +0300

    Исходный файл

---
+++ src/main/java/pro/sky/spring/sky/pro/calculatorHomeWork23/ApplicationCalculator.java
@@ -0,0 +1,13 @@
+package pro.sky.spring.sky.pro.calculatorHomeWork23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCalculator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCalculator.class, args);
+	}
+
+}
